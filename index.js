67fdd7feff9261d118c4bc86782436f1a0811f3f
ReplacedSpace17 <replacedspace17@gmail.com>
2024-07-23 12:55:42 -0600

    s

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const io = socketIo(server);
 
 // Configura CORS
 const corsOptions = {
-  origin: ['https://segucom.mx', 'http://localhost:3001',  'http://localhost:3000','http://localhost:3002', 'https://segubackend.com:3000',
+  origin: ['https://segucom.mx', 'http://localhost:3001',  'http://localhost:3000','http://localhost:3002', 'https://segubackend.com:3000','https://segubackend.com','https://localhost:3001',
      'https://localhost:3000', 'http://192.168.1.90'
   ],
   optionsSuccessStatus: 200
