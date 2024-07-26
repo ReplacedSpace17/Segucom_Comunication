dc76c62cf074b74ee17d89cd8d791372c6f99307
ReplacedSpace17 <replacedspace17@gmail.com>
2024-07-26 15:51:58 -0600

    d

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const corsOptions = {
   origin: [
     'https://segucom.mx',
     'http://localhost:3001',
+    'http://localhost:3000',
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'http://192.168.1.68',
